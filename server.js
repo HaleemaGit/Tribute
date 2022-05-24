eb48cbda15613966abb6a8cf8b135114ef3b6758
HaleemaGit <mustaphahaleema3@gmail.com>
2022-05-24 11:05:51 +0100

    troubleshooting

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+// var app = express();
+
+
+// app.set('port', (process.env.PORT || 5000))
+// app.use(express.static('public'))
+
+// Load Node modules
+// var express = require('express');
+// Initialise Express
 var app = express();
-
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static('public'))
-
+// Render static files
+app.use(express.static('public'));
+// Port website will run on
+app.listen(8080);
 
 
